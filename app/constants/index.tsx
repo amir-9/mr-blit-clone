3430847c6fcabd3f966a3f1d3effa4b965fe7902
amirHossein <amir969492@gmail.com>
2023-05-30 15:27:24 +0430

    constans file name changed to index & added bg images info in it

--- app/constants/index.tsx
+++ app/constants/index.tsx
@@ -32,3 +32,36 @@ export const navbarItems: Array<NavItemType> = [
     navbarIcon: <NavbarIcons name="car" fill="#000" />,
   },
 ];
+
+export const headerBgImageItems = [
+  {
+    name: "flight",
+    width: 497,
+    height: 114,
+    src: "/images/header/flight-desktop.svg",
+  },
+  {
+    name: "train",
+    width: 629,
+    height: 79,
+    src: "/images/header/train-desktop.svg",
+  },
+  {
+    name: "bus",
+    width: 327,
+    height: 93,
+    src: "/images/header/bus-desktop.svg",
+  },
+  {
+    name: "taxi",
+    width: 224,
+    height: 74,
+    src: "/images/header/taxi-desktop.svg",
+  },
+  {
+    name: "ship",
+    width: 399,
+    height: 173,
+    src: "/images/header/ship-desktop.svg",
+  },
+];
